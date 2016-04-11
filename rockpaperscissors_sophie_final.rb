c2a8fbab132d68234fd46ed6f2b3ae37aa4b1977
Jessica <Jessica.Lair@hotmail.com>
2016-04-11 19:39:28 -0400

    Added a comment

--- rockpaperscissors_sophie_final.rb
+++ rockpaperscissors_sophie_final.rb
@@ -3,16 +3,16 @@ puts "Let's play rock paper scissors! We will be playing 3 rounds. The best of 3
 computer_won = 0
 player_won = 0
 playagain = true
-	
-sleep (1)
 
+sleep (1)
+#loop
 while playagain == true do
-while computer_won + player_won < 3 do 
+while computer_won + player_won < 3 do
 
 	computer = options.sample
 	puts "Please choose rock, paper or scissors:"
 	player = gets.chomp.downcase
-	
+
 	# if player plays rock
 	if player == options[0]
 		# if computer plays rock
@@ -42,7 +42,7 @@ while computer_won + player_won < 3 do
 				puts "You: #{player_won}"
 			end
 			sleep (1)
-			
+
 		end
 	# if player plays paper
 	elsif player == options[1]
@@ -75,7 +75,7 @@ while computer_won + player_won < 3 do
 			sleep (1)
 		end
 	# if player plays scissors
-	elsif player == options[2] 
+	elsif player == options[2]
 		# if computer plays rock
 		if computer == options[0]
 			puts "Computer played: #{computer}"
@@ -97,31 +97,31 @@ while computer_won + player_won < 3 do
 				puts "Computer: #{computer_won}"
 				puts "You: #{player_won}"
 			end
-			sleep (1)                                 
+			sleep (1)
 		# if computer plays scissors
 		elsif computer == options[2]
 			puts "Computer played: #{computer}"
 			puts "Uh oh it's a tie, this round doesn't count!"
 			sleep (1)
 		end
-	
+
 	# if player plays anything else
-	else   
+	else
 		puts "Are you sure you spelled that right?"
 		sleep (1)
-	
+
 	end
 end
 
 puts "Please wait for the results... *drumroll*"
-sleep (1)	
+sleep (1)
 puts "Computer won #{computer_won} out of 3 rounds"
 puts "You won #{player_won} out of 3 rounds"
 if computer_won > player_won
 	puts "You lost the game, sorry!"
 else
-	puts "You won the game! Congratulations!" 
-end	
+	puts "You won the game! Congratulations!"
+end
 
 puts "Would you like to play again? Please say yes or no."
 play=gets.chomp.downcase
@@ -130,11 +130,11 @@ if play == "yes"
 	playagain = true
 	computer_won = 0
 	player_won = 0
-elsif play == "no" 
+elsif play == "no"
 	puts "Thanks for playing!"
 	playagain = false
 else
 	puts "I take that as a no. Thanks for playing!"
 	playagain = false
 end
-end 
+end
